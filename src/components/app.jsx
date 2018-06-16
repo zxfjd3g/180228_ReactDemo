b2fc97b32ef71ebe6595dc9343146c2a7b563bae
zxfjd3g <258147149@qq.com>
2018-06-16 16:00:03 +0800

    路由

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,45 @@
 import React, {Component} from 'react'
-import Search from './search'
-import Main from './main'
+import {NavLink, Route, Switch, Redirect} from 'react-router-dom'
+import About from '../views/about'
+import Home from '../views/home'
 
 export default class App extends Component {
-
-  render () {
+  render() {
     return (
-      <div className="container">
-        <Search/>
-        <Main/>
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/*指定路由链接*/}
+              <NavLink className='list-group-item' to="/about" activeClassName='activeClass'>About</NavLink>
+              <NavLink className='list-group-item' to="/home" activeClassName='activeClass'>Home</NavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/*显示路由组件的位置*/}
+                <Switch>{/*只显示当前匹配的某个路由组件*/}
+                  <Route path='/about' component={About}/>
+                  <Route path='/home' component={Home}/>
+                  <Redirect to='/about'/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+/*
+路由链接: NavLink
+注册路由: Route
+ctrl+shift+f  format
+*/
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import App from './components/app'
-import './index.css'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+), document.getElementById('root'))
